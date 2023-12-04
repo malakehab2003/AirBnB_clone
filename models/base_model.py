13f4f5077ad5e9b5ffacf3733a7abc46c0a14e2f
DJ-Man-2099 <davidmesak@hotmail.com>
2023-12-04 20:06:38 +0200

    Add instance from dict, with test cases

--- models/base_model.py
+++ models/base_model.py
@@ -12,20 +12,28 @@ class BaseModel:
     defines all common attributes/methods for other classes
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initialization
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if ["created_at", "updated_at"].count(key) == 0:
+                        self.__setattr__(key, value)
+                    else:
+                        self.__setattr__(key, datetime.fromisoformat(value))
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
         String Representation of class
         """
         return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
-    
+
     def save(self):
         """
         updates the public instance attribute updated_at with the current datetime
@@ -38,6 +46,6 @@ class BaseModel:
         """
         temp_dict = {}
         for key, value in self.__dict__.items():
-            temp_dict[key] = value if ["created_at","updated_at"].count(key) == 0\
+            temp_dict[key] = value if ["created_at", "updated_at"].count(key) == 0\
                 else self.__getattribute__(key).isoformat()
         return temp_dict
---
+++ tests/test_models/test_base_model_dict.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+from datetime import datetime
+import unittest
+from models.base_model import BaseModel, __doc__
+from help_functions.helpers import Helpers
+
+
+class TestBaseModel(unittest.TestCase):
+    """
+    Class for Unit Testing BaseModel()
+
+    this class checks for every detail in the class
+    """
+
+    def test_base_init_kwargs(self):
+        """
+        kwargs test
+        """
+        helpers = Helpers()
+        my_model = BaseModel()
+        self.assertIsInstance(my_model, BaseModel)
+        self.assertIsInstance(my_model.id, str)
+        self.assertIsInstance(my_model.created_at, datetime)
+        self.assertIsInstance(my_model.updated_at, datetime)
+        my_model.name = "My First Model"
+        my_model.my_number = 89
+        my_model.save()
+        my_model_json = my_model.to_dict()
+        my_new_model = BaseModel(**my_model_json)
+        my_dict = {
+            "id": my_model.id,
+            'created_at': my_model.created_at,
+            'updated_at': my_model.updated_at,
+            "name": "My First Model",
+            "my_number": 89
+        }
+        self.assertEqual(my_new_model.id, my_model.id)
+        self.assertEqual(my_new_model.created_at, my_model.created_at)
+        self.assertEqual(my_new_model.updated_at, my_model.updated_at)
+        self.assertEqual(my_new_model.name, my_model.name)
+        self.assertEqual(my_new_model.my_number, my_model.my_number)
+        self.assertIsNot(my_new_model, my_model)
+        self.assertIsInstance(my_new_model, BaseModel)
+        self.assertIsInstance(my_new_model.id, str)
+        self.assertIsInstance(my_new_model.created_at, datetime)
+        self.assertIsInstance(my_new_model.updated_at, datetime)
+        helpers.stdout(lambda: print(my_new_model),
+                       f"[BaseModel] ({my_model.id}) {my_dict}\n")
