b1500516c31132d668795db88d5abd7151bbcec1
malakehab2003 <lokaehab2003@gmail.com>
2023-12-09 21:31:38 +0200

    try update

--- console.py
+++ console.py
@@ -21,6 +21,7 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, s):
         """function on EOF
         """
+        print()
         return True
 
     def help_quit(self):
@@ -162,47 +163,25 @@ class HBNBCommand(cmd.Cmd):
         print("Deletes an instance based on the class name and id")
         print("Usage: destroy <model_name> <id>\n")
 
-    def do_update(self, s):
-        """Updates an instance based on the class name and id
-        by adding or updating attribute
+    def do_update(self, arg):
+        """update data of an instanse
         """
-        args = shlex.split(s)
-        if not args or not args[0]:
-            print("** class name missing **")
-            return
-        class_name = args[0]
-        from models.base_model import storage
-        from models.models_dict import all_models
-        if class_name not in all_models.keys():
-            print("** class doesn't exist **")
-            return
-        if len(args) < 2 or not args[1]:
-            print("** instance id missing **")
-            return
-        id = args[1]
-        all_objects = storage.all()
-        key = f"{class_name}.{id}"
-        if key not in all_objects.keys():
-            print("** no instance found **")
-            return
-        if len(args) < 3 or not args[2]:
+        class_id = handle_error(arg)
+        arg_list = arg.split()
+        all_objs = storage.all()
+        if len(arg_list) < 3:
             print("** attribute name missing **")
-            return
-        attrib = args[2]
-        if len(args) < 4 or not args[3]:
+        elif len(arg_list) < 4:
             print("** value missing **")
-            return
-        value = args[3]
-        if value[0] == '"':
-            value = value[1:-1]
-        if value.isdigit():
-            casted_arg = int(value)
-        elif is_float(value):
-            casted_arg = float(value)
         else:
-            casted_arg = str(value)
-        setattr(all_objects[key], args[2], casted_arg)
-        storage.save()
+            if arg_list[3][1:-1].isdigit():
+                casted_arg = int(arg_list[3][1:-1])
+            elif is_float(arg_list[3][1:-1]):
+                casted_arg = float(arg_list[3][1:-1])
+            else:
+                casted_arg = str(arg_list[3][1:-1])
+            setattr(all_objs[class_id], arg_list[2], casted_arg)
+            storage.save()
 
     def help_update(self):
         """shows what update does
@@ -211,6 +190,31 @@ class HBNBCommand(cmd.Cmd):
               " by adding or updating attribute")
         print('update <class name> <id> <attribute name>' +
               '"<attribute value>"\n')
+
+class_dict = {
+        "BaseModel": BaseModel
+    }
+
+def handle_error(arg):
+    """handle some errors
+    """
+    arg_list = arg.split()
+
+    if len(arg_list) == 0 or arg_list is None:
+        print("** class name missing **")
+    # third and final place
+    elif arg_list[0] not in class_dict.keys():
+        print("** class doesn't exist **")
+    elif len(arg_list) < 2:
+        print("** instance id missing **")
+    else:
+        class_id = "{}.{}".format(arg_list[0], arg_list[1])
+        all_objs = storage.all()
+        if class_id in all_objs:
+            return class_id
+        else:
+            print("** no instance found **")
+            return None
 
 def is_float(value):
     """check if number is float
