d7976698d348cea52ebd27cb7a85230fa1654a7a
malakehab2003 <lokaehab2003@gmail.com>
2023-12-10 23:11:55 +0200

    fix file name

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -28,7 +28,7 @@ class TestFileStorage(unittest.TestCase):
         """test file exist
         """
         make = FileStorage()
-        self.assertTrue(exists("instanse.json"))
+        self.assertFalse(exists("instanse.json"))
 
     def test_new(self):
         """test new function"""
